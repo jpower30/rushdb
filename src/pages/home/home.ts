ef439465fe710d39e73be3421ea7ed2db06f5758
Mayhul Arora <mayhul99@gmail.com>
2018-04-07 16:43:12 -0400

    Log in page UI done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { regexValidators } from '../validators/validator';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  credentialsForm: FormGroup;
 
+  constructor(public navCtrl: NavController,
+              private formBuilder: FormBuilder) {
+
+    this.credentialsForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.pattern(regexValidators.email), Validators.required])],
+      password: ['', Validators.compose([Validators.pattern(regexValidators.password), Validators.required])]
+    });
   }
 
+  onSignIn() {
+    if (this.credentialsForm.valid) {
+      console.log('Email: ' +
+        this.credentialsForm.controls['email'].value);
+      console.log('Password: ' +
+        this.credentialsForm.controls['password'].value);
+    }
+  }
 }
